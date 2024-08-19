90cb3e197f5de597ed65d2600771ef6b8b04dcbc
Ananas <ananas@ananas.moe>
2024-08-19 02:10:41 +0200

    feat, refactor: add httpcat, status command

--- aninas/__main__.py
+++ aninas/__main__.py
@@ -1,12 +1,14 @@
 import asyncio
 import disnake
-import sys
 
 from disnake.ext import commands    
+from datetime import datetime
 
 from .constant import BOT_TOKEN, Colours
 from .utils import embed
 from .database import Redis
+
+from . import __version__
 
 class Aninas(commands.InteractionBot):
     def __init__(self):
@@ -16,21 +18,26 @@ class Aninas(commands.InteractionBot):
         disnake.Embed.set_default_colour(Colours.cerise)
 
         self._redis = Redis()
+        self._started = int(datetime.now().timestamp())
 
-        super().__init__(intents=intents)
+        super().__init__(intents=intents, activity = disnake.Activity(name = f"v{__version__}", state = "Haiiii :3"))
 
     @property
     def redis(self) -> Redis:
         return self._redis
 
+    @property
+    def started(self) -> int:
+        return self._started
+
     async def on_ready(self):
-        print(f"logged in as {str(self.user)}")
+        print(f"Ready: {str(self.user)}")
     
     async def close(self):
         await super().close()
 
         await self._redis.close()
-    
+
     async def on_slash_command_error(self, inter: disnake.CommandInteraction, error: commands.CommandInvokeError):
         if inter.response.is_done():
             return await inter.followup.send(embed=embed.error("Something went wrong", error.original))
--- aninas/plugins/utils.py
+++ aninas/plugins/utils.py
@@ -2,14 +2,80 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ..utils.types.anime_girls import AnimeGirl
+    ...
 
 from disnake.ext import commands, plugins
 
 import disnake
+import random
 import base64
 
+from ..constant import Emojis
+from ..utils.embed import line_fix
+
+from platform import python_version
+from disnake import __version__ as disnake_version
+from .. import __version__ as bot_version
+
 plugin = plugins.Plugin()
+HTTP_STATUS_CODES = [
+    100, 101, 102, 103,
+    200, 201, 202, 203, 204, 205, 206, 207, 208, 214, 226,
+    300, 301, 302, 303, 304, 305, 307, 308,
+    400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 
+    414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431
+]
+
+@plugin.slash_command(description="Status about aninas")
+async def status(
+    inter: disnake.CommandInteraction
+):
+    embed = disnake.Embed(
+        title = "Status",
+        description = "Source: [Codeberg](https://codeberg.org/bananas/aninas)"
+    )
+
+    embed.add_field(
+        f"{Emojis.zap} __Version__",
+        line_fix(f"""
+        - Python: `{python_version()}`
+        - Disnake: `{disnake_version}`
+        - Aninas: `{bot_version}`
+        """)
+    )
+
+    embed.add_field(
+        f"{Emojis.notepad} __Stats__",
+        line_fix(f"""
+        - Guilds: `{len(plugin.bot.guilds)}`
+        - Uptime: <t:{plugin.bot.started}:R>
+        """)
+    )
+
+    embed.set_author(
+        name = "Ananas",
+        url = "https://ananas.moe",
+        icon_url = "https://avatars.githubusercontent.com/u/132799819"
+    )
+
+    embed.set_image(
+        "https://cdn.ananas.moe/mashiro_shiina.png"
+    )
+
+    embed.set_footer(text = "uwu" if random.randint(1, 5) == 4 else "")
+
+    await inter.response.send_message(embed=embed)
+
+@plugin.slash_command(description="Get a image from http.cat")
+async def httpcat(
+    inter: disnake.CommandInteraction,
+    status_code: int = commands.Param(description="HTTP status code")
+):
+    if status_code not in HTTP_STATUS_CODES:
+        await inter.response.send_message("That is not a valid status code", ephemeral=True)
+        return
+
+    await inter.response.send_message(f"https://http.cat/{status_code}")
 
 @plugin.slash_command(name="base64")
 async def base64_command(inter):
