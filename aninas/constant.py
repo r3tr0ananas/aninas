561120be142afb62f7e3916ec98bd9caff6d8d7a
Ananas <ananas@ananas.moe>
2024-09-15 23:11:59 +0200

    refactor: put resources into aninas/assets

--- aninas/constant.py
+++ aninas/constant.py
@@ -41,9 +41,9 @@ class Colours:
 
     cerise = 0xDE3163
 
-THIS_IS_TEMPLATE = config("THIS_IS_TEMPLATE", default = "resources/this_is.jpg")
-THIS_IS_FONT = config("FONT", default = "resources/poppins.ttf")
-QUOTE_FONT = config("THIS_IS_TEMPLATE", default = "resources/ubuntu.ttf")
+THIS_IS_TEMPLATE = config("THIS_IS_TEMPLATE", default = "aninas/assets/this_is.jpg")
+THIS_IS_FONT = config("FONT", default = "aninas/assets/poppins.ttf")
+QUOTE_FONT = config("THIS_IS_TEMPLATE", default = "aninas/assets/ubuntu.ttf")
 REDIS = config("REDIS", default = "redis://localhost:6379")
 LIMIT_CHAR = config("LIMIT_CHAR", default = 240)
 CODEBERG_KEY = config("CODEBERG_KEY", cast = str)
